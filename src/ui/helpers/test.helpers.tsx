9c0c20378dfa10a61de96925e7b151464b214241
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-04-12 20:45:08 +0700

    feat(ui): добавляет вспомогательные функции для тестирования

---
+++ src/ui/helpers/test.helpers.tsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme } from '../styles'
+
+function renderWithTheme(toRender: JSX.Element) {
+	return render(<ThemeProvider theme={darkTheme}>{toRender}</ThemeProvider>)
+}
+
+function getStyleObject(
+	toRender: (args: any) => { type: { styledComponentId: string } }
+): CSSStyleDeclaration {
+	return window.getComputedStyle(
+		document.getElementsByClassName(toRender({}).type.styledComponentId)[0]
+	)
+}
+
+export { renderWithTheme, getStyleObject }
