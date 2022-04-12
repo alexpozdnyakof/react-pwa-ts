b5ee622a312ed7c5a56f7b8a7f8ddb7f05f4bf58
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-04-13 03:43:40 +0700

    feat(ui/helpers): добавляет вспомогательные функции storybook

---
+++ src/ui/helpers/storybook.helpers.tsx
@@ -0,0 +1,42 @@
+import { Space } from '../styles'
+
+const Text = () => ({
+	control: 'text',
+})
+const Color = () => ({
+	control: 'color',
+})
+
+const Select = (
+	options: Array<string | number>,
+	mapping?: Record<string, any>
+) => ({
+	control: 'select',
+	options,
+	mapping,
+})
+
+const SelectWithNone = (options: Array<string>) =>
+	Select(['none', ...options], { none: undefined })
+
+const ReusableBlock = () => ({
+	padding: SelectWithNone(Object.values(Space)),
+	paddingTop: SelectWithNone(Object.values(Space)),
+	paddingRight: SelectWithNone(Object.values(Space)),
+	paddingBottom: SelectWithNone(Object.values(Space)),
+	paddingLeft: SelectWithNone(Object.values(Space)),
+
+	margin: SelectWithNone(Object.values(Space)),
+	marginTop: SelectWithNone(Object.values(Space)),
+	marginRight: SelectWithNone(Object.values(Space)),
+	marginBottom: SelectWithNone(Object.values(Space)),
+	marginLeft: SelectWithNone(Object.values(Space)),
+})
+
+export const StoryBookControl = {
+	Text,
+	Color,
+	Select,
+	SelectWithNone,
+	ReusableBlock,
+}
