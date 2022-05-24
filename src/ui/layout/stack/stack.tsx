df29a11261b81a20c285dac69434c64e96e62df7
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-05-24 21:22:38 +0700

    feat(stack): добавляет перенаправление ссылки

--- src/ui/layout/stack/stack.tsx
+++ src/ui/layout/stack/stack.tsx
@@ -1,26 +1,36 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import React, { ForwardedRef, forwardRef } from 'react'
 import { Block } from '../../lib'
 import { SPACE_UNIT } from './constraints'
 import { StackProps } from './types'
 
-export default function Stack({
-	space = 0,
-	spaceUnit = SPACE_UNIT,
-	testId,
-	children,
-	...props
-}: StackProps) {
-	const gap = space * spaceUnit
+const Stack = forwardRef<HTMLDivElement, StackProps>(
+	(
+		{
+			space = 0,
+			spaceUnit = SPACE_UNIT,
+			testId,
+			children,
+			...props
+		}: StackProps,
+		ref: ForwardedRef<HTMLDivElement>
+	) => {
+		const gap = space * spaceUnit
 
-	return (
-		<Block
-			{...props}
-			display='grid'
-			gridAutoFlow='row'
-			gap={gap}
-			testId={testId}
-		>
-			{children}
-		</Block>
-	)
-}
+		return (
+			<Block
+				{...props}
+				display='grid'
+				gridAutoFlow='row'
+				gap={gap}
+				testId={testId}
+				ref={ref}
+			>
+				{children}
+			</Block>
+		)
+	}
+)
+
+Stack.displayName = 'Stack'
+export default Stack
