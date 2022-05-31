1a9c882d97cb6f893f970127184307ceac85de65
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-05-31 23:06:27 +0700

    refactor(reoder): удаляет react-dnd

--- src/ui/pages/todos/reorder/drag-item.ts
+++
@@ -1,6 +0,0 @@
-import { Todo, TodoList } from '../../../../domain'
-
-export type TodoDragItem = {
-	columnId: string
-} & Todo
-export type DragItem = TodoList | TodoDragItem
--- src/ui/pages/todos/reorder/index.ts
+++
@@ -1,1 +0,0 @@
-export type { DragItem } from './drag-item'
