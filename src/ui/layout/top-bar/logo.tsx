46754be55ab267c6761657adafb13679c2694394
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-06-03 05:46:09 +0700

    feat: добавляет logo

--- src/ui/layout/top-bar/logo.tsx
+++ src/ui/layout/top-bar/logo.tsx
@@ -1,5 +1,5 @@
 import { Block } from '../../lib'
 
 export default function Logo() {
-	return <Block as='img' src='logo.png' width={136} />
+	return <Block as='img' src='logo.png' width={136} testId='logo' />
 }
