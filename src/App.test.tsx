900d80038a1cd28d920e47a3fa048bd765e71891
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-06-06 21:44:10 +0700

    test(App): переводит тесты роутера на memoryRouter вместо history

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,55 +1,40 @@
-import { createMemoryHistory, MemoryHistory } from 'history'
 import { render } from '@testing-library/react'
-import { Router } from 'react-router-dom'
+import { MemoryRouter } from 'react-router-dom'
 import App from './App'
 import AppProviders from './providers'
 
 describe('App', () => {
-	let history!: MemoryHistory | undefined
-
-	beforeEach(() => {
-		history = createMemoryHistory()
-	})
-	afterEach(() => {
-		history = undefined
-	})
 	it('should render todo page', () => {
-		history?.push('/todos')
-
 		const { getByTestId } = render(
-			<Router location={history?.location!} navigator={history!}>
+			<MemoryRouter initialEntries={['/todos']}>
 				<AppProviders>
 					<App />
 				</AppProviders>
-			</Router>
+			</MemoryRouter>
 		)
 
 		expect(getByTestId('todo-page')).toBeInTheDocument()
 	})
 
 	it('should render sign-in page', () => {
-		history?.push('/sign-in')
-
 		const { getByTestId } = render(
-			<Router location={history?.location!} navigator={history!}>
+			<MemoryRouter initialEntries={['/sign-in']}>
 				<AppProviders>
 					<App />
 				</AppProviders>
-			</Router>
+			</MemoryRouter>
 		)
 
 		expect(getByTestId('signinpage')).toBeInTheDocument()
 	})
 
 	it('should render sign-up page', () => {
-		history?.push('/sign-up')
-
 		const { getByTestId } = render(
-			<Router location={history?.location!} navigator={history!}>
+			<MemoryRouter initialEntries={['/sign-up']}>
 				<AppProviders>
 					<App />
 				</AppProviders>
-			</Router>
+			</MemoryRouter>
 		)
 
 		expect(getByTestId('signuppage')).toBeInTheDocument()
