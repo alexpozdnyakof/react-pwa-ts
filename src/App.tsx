8c7e8a6bae0e76408264dbaa456abff8ce177687
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-06-15 05:27:46 +0700

    feat(routing): добавляет lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
+import { lazy, PropsWithChildren, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { TopBar } from './ui/layout'
-import SignInPage from './ui/pages/sign-in/sign-in'
-import SignUpPage from './ui/pages/sign-up/sign-up'
-import { TodosPage } from './ui/pages/todos'
+import { Block } from './ui/lib'
+
+const TodosPage = lazy(
+	() =>
+		import(
+			/* webpackChunkName: "TodosPage", webpackPrefetch: true  */ './ui/pages/todos/todos-page'
+		)
+)
+const SignInPage = lazy(
+	() =>
+		import(
+			/* webpackChunkName: "SignInPage", webpackPrefetch: true  */ './ui/pages/sign-in/sign-in'
+		)
+)
+const SignUpPage = lazy(
+	() =>
+		import(
+			/* webpackChunkName: "SignUpPage", webpackPrefetch: true  */ './ui/pages/sign-up/sign-up'
+		)
+)
+
+const SuspendedRoute = ({ children }: PropsWithChildren<{}>) => (
+	<Suspense fallback={<Block>Loading ...</Block>}> {children}</Suspense>
+)
 
 function App() {
 	return (
 		<>
-			<TopBar />
-			<Routes>
-				<Route path='/todos' element={<TodosPage />} />
-				<Route path='/sign-in' element={<SignInPage />} />
-				<Route path='/sign-up' element={<SignUpPage />} />
-			</Routes>
+			<TopBar overlay='overlay' />
+			<SuspendedRoute>
+				<Routes>
+					<Route path='/todos' element={<TodosPage />} />
+					<Route path='/sign-in' element={<SignInPage />} />
+					<Route path='/sign-up' element={<SignUpPage />} />
+				</Routes>
+			</SuspendedRoute>
 		</>
 	)
 }
